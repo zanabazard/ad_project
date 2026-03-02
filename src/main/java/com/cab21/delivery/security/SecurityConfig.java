26ce8a9656eb33aca6224e737be69a080344e598
zanabazar.i <zanabazar.i@nomin.net>
2026-03-02 11:22:16 +0800

    changes

--- src/main/java/com/cab21/delivery/security/SecurityConfig.java
+++ src/main/java/com/cab21/delivery/security/SecurityConfig.java
@@ -45,10 +45,8 @@ public class SecurityConfig {
         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // ✅ important
 
         .requestMatchers(HttpMethod.POST,
-            "/api/user/create",
-            "/cab21/api/user/create",
             "/api/auth/login",
-            "/cab21/api/auth/login",
+            "/api/user/create",
             "/api/rides/checklist/grid",
             "/api/user/change-password"
         ).permitAll()
